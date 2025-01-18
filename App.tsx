b4bf03f1c6a444d3d567f36cccc0a00e7d039360
jarrenoh <jarrenoh@gmail.com>
2025-01-18 18:05:26 +0800

    added login screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,27 @@
-import 'react-native-gesture-handler';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import LoginScreen from 'screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import RootStack from './navigation';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <RootStack />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
