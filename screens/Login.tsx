f12602c3134c4c287f3b34eadaae5fe641e47d0e
jarrenoh <jarrenoh@gmail.com>
2025-01-18 18:45:25 +0800

    signup works

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import React, {useState }from 'react'
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth'
 import { FIREBASE_AUTH } from 'firebase'
 
 const LoginScreen = () => {
@@ -8,11 +9,85 @@ const LoginScreen = () => {
   const [loading, setLoading] = useState(false)
   const auth = FIREBASE_AUTH
 
-  return (
-    <View>
+  const signup = () => {
+    setLoading(true)
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log('Signed up')
+      })
+      .catch((error) => {
+        console.log('Error:', error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  const signin = () => {
+    setLoading(true)
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log('Logged in')
+      })
+      .catch((error) => {
+        console.log('Error:', error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+
+return (
+    <View style = {styles.container}>
       <Text>LoginScreen</Text>
+      <TextInput
+        placeholder = "Email"
+        value = {email}
+        onChangeText = {setEmail}
+        autoCapitalize='none'
+        style = {{borderWidth: 1, padding: 10, margin: 10, width: 200}}
+      />
+      <TextInput
+        secureTextEntry = {true}
+        placeholder = "Password"
+        value = {password}
+        onChangeText = {setPassword}
+        style = {{borderWidth: 1, padding: 10, margin: 10, width: 200}}
+      />
+      <Button
+        title = "Login"
+        onPress = {() => {
+          setLoading(true)
+          signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+              console.log('Logged in')
+            })
+            .catch((error) => {
+              console.log('Error:', error)
+            })
+            .finally(() => {
+              setLoading(false)
+            })
+        }}
+        disabled = {loading}
+      />
+
+      <Button
+        title = "Sign Up"
+        onPress = {signup}
+        disabled = {loading}
+      />
     </View>
   )
 }
 
 export default LoginScreen
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center' as 'center',
+    alignItems: 'center' as 'center'
+  }
+}
